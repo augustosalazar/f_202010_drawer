24d5eb021668b3e81c3f1f419727d7b29804e9ae
augustosalazar <augustosalazar@uninorte.edu.co>
2020-03-06 18:07:07 -0500

    first commit

---
+++ lib/widgets/my_drawer.dart
@@ -0,0 +1,57 @@
+import 'package:f_simple_button/screens/contactpage.dart';
+import 'package:f_simple_button/screens/drawer_home.dart';
+import 'package:f_simple_button/screens/eventpage.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 
+      Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+               onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+               // Navigator.pushReplacementNamed(context, Routes.contacts);
+
+                Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => ContactsPage()));
+
+
+              },          
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+                                Navigator.of(context).pop();
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => EventsPage()));
+              },
+            ),
+          ],
+        ),
+      );
+
+
+  }
+}
